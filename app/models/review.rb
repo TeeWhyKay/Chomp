91ac9772802e3e63de6555ece5db8c39ce0820df
Min Xuan <hi@ngminxuan.com>
2021-09-21 16:34:11 +0800

    added validation for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  validates :title, presence: true, length: { in: 3..30 }
+  validates :content, presence: true, length: { in: 5..300 }
+  validates :rating, presence: true
 end
