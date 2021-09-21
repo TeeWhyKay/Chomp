50a3e59b0394aad24e22b629b454c94c5562e80a
Min Xuan <hi@ngminxuan.com>
2021-09-21 17:50:43 +0800

    added reviews controller & reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # test
-  resources :restaurants, only: [ :index, :show ]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
 end
