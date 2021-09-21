2b0f45948982bfbbf61496edae16ba786e37f054
Min Xuan <hi@ngminxuan.com>
2021-09-21 18:00:02 +0800

    added boilerplate for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
