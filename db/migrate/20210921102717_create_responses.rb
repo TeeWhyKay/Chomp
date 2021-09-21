506fd0a27cead8a53bfc6c234d98c8a28f793ba5
YongKeeTan <yongkee.t@gmail.com>
2021-09-21 18:31:59 +0800

    created validations for responses

---
+++ db/migrate/20210921102717_create_responses.rb
@@ -0,0 +1,16 @@
+class CreateResponses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :responses do |t|
+      t.integer :budget
+      t.string :location
+      t.string :email
+      t.string :cuisine
+      t.float :latitude
+      t.float :longitude
+      t.references :user, null: false, foreign_key: true
+      t.references :chompsession, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
