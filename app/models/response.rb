c6803535ab3820e8971f3a376e85f44c7f62ca92
YongKeeTan <yongkee.t@gmail.com>
2021-09-21 18:35:43 +0800

    added validation for email, blank/nil responses are allowed

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,5 @@ class Response < ApplicationRecord
   belongs_to :chompsession
   validates :budget, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 5 }
   validates :location, presence: true
+  validates :email, allow_blank: true
 end
