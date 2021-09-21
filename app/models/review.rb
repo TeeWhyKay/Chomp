80fa79b85d52b76a51bfbb86c0b14d3b7f3174b2
Min Xuan <hi@ngminxuan.com>
2021-09-21 18:00:48 +0800

    remove presence true on title and content

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  validates :title, presence: true, length: { in: 3..30 }
-  validates :content, presence: true, length: { in: 5..300 }
+  validates :title, length: { in: 3..30 }
+  validates :content, length: { in: 5..300 }
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 2 }
 end
