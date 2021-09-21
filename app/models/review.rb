d558a4d0f1e5677d13147a51361f2efd6618fee7
Min Xuan <hi@ngminxuan.com>
2021-09-21 18:02:24 +0800

    changed range to 1-5 for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
 
   validates :title, length: { in: 3..30 }
   validates :content, length: { in: 5..300 }
-  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 2 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
