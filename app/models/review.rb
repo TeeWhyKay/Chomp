a4a5a55bd0089e2551d1ff630ff1a9d8e97e4fef
Min Xuan <hi@ngminxuan.com>
2021-09-21 17:17:58 +0800

    added range

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
 
   validates :title, presence: true, length: { in: 3..30 }
   validates :content, presence: true, length: { in: 5..300 }
-  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 2 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 2 }
 end
