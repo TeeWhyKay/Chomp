3fdfc63904e3f86b70120f776e13a3412b69805c
YongKeeTan <yongkee.t@gmail.com>
2021-09-22 15:24:53 +0800

    adding attached image to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :chomp_sessions
+  has_one_attached :image
   validates :name, :address, presence: true
 end
