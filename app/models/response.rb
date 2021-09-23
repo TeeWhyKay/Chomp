ecf9d7499536d7b20f0556d459042a37fcf384e8
YongKeeTan <yongkee.t@gmail.com>
2021-09-23 13:19:22 +0800

    restaurant and response geocoding

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,9 @@ class Response < ApplicationRecord
   belongs_to :chompsession
   validates :budget, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 5 }
   validates :location, presence: true
+  geocoded_by :location,
+              latitude: :fetched_latitude,
+              longitude: :fetched_longitude
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :geocode, unless: ->(obj){ obj.latitude.present? and obj.longitude.present? }
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,9 @@ class Restaurant < ApplicationRecord
   has_many :chomp_sessions
   has_one_attached :image
   validates :name, :address, presence: true
+  geocoded_by :address,
+              latitude: :fetched_latitude,
+              longitude: :fetched_longitude
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :geocode, unless: ->(obj){ obj.address.present? and obj.latitude.present? and obj.longitude.present? }
 end
