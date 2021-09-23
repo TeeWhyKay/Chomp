ff879bc693765659ab648eaccf753693db93c1ed
Min Xuan <hi@ngminxuan.com>
2021-09-23 12:30:39 +0800

    changed email subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: "You've created a new Chomp")
+    mail(to: @user.email, subject: "You've created a Chomp account")
     # This will render a view in `app/views/user_mailer`!
   end
 end
