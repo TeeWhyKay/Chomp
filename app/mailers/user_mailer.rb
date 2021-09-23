05f4e86a4d0e624ceeae383681e27a1048926086
Min Xuan <hi@ngminxuan.com>
2021-09-23 10:41:27 +0800

    test mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: "You've created a new Chomp")
     # This will render a view in `app/views/user_mailer`!
   end
 end
