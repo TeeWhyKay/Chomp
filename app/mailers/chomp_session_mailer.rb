6a90b07b0c70ebe488663587b696966c42e495cd
Min Xuan <hi@ngminxuan.com>
2021-09-24 15:50:42 +0800

    refactor

--- app/mailers/chomp_session_mailer.rb
+++ app/mailers/chomp_session_mailer.rb
@@ -1,4 +1,5 @@
 class ChompSessionMailer < ApplicationMailer
+  before_action :set_chomp_session
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -6,20 +7,22 @@ class ChompSessionMailer < ApplicationMailer
   #   en.chomp_session_mailer.create_chomp.subject
   #
   def create_chomp
-    @chomp_session = params[:chomp_session]
-
     mail(
-      to:       @chomp_session.user.email,
-      subject:  "Chomp Session: '#{@chomp_session.name}' is created!"
+      to: @chomp_session.user.email,
+      subject: "Chomp Session: '#{@chomp_session.name}' is created!"
     )
   end
 
   def update_chomp
-    @chomp_session = params[:chomp_session]
-
     mail(
-      to:       @chomp_session.user.email,
-      subject:  "Chomp Session: '#{@chomp_session.name}' is updated!"
+      to: @chomp_session.user.email,
+      subject: "Chomp Session: '#{@chomp_session.name}' is updated!"
     )
+  end
+
+  private
+
+  def set_chomp_session
+    @chomp_session = params[:chomp_session]
   end
 end
