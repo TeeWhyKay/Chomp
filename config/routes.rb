7a0ce175a0d3071719abb3fd751a38d13e703825
Min Xuan <hi@ngminxuan.com>
2021-09-24 10:59:24 +0800

    basic sidekiq setup from guide

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,10 @@ Rails.application.routes.draw do
     resources :responses, only: [:create]
     get 'success', to: 'chomp_sessions#success'
   end
+
+  # Sidekiq Web UI, only for admins.
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
