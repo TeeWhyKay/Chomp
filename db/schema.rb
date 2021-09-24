85ef0f7f3f6afe4f990aa98f40582d0440a8338f
Min Xuan <hi@ngminxuan.com>
2021-09-24 15:03:47 +0800

    remove email from responses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_030033) do
+ActiveRecord::Schema.define(version: 2021_09_24_070226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_09_24_030033) do
   create_table "responses", force: :cascade do |t|
     t.integer "budget"
     t.string "location"
-    t.string "email"
     t.string "cuisine"
     t.float "latitude"
     t.float "longitude"
