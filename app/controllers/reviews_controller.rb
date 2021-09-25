e0ce30659008c9c9f5d1564b056fb126139ab532
Min Xuan <hi@ngminxuan.com>
2021-09-25 19:10:51 +0800

    able to update form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
   def create
     # Creating new review
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.user = current_user
@@ -27,8 +28,8 @@ class ReviewsController < ApplicationController
     @review.update(review_params)
 
     respond_to do |format|
-      format.html { redirect_to restaurant_path(@review.restaurant) }
-      format.text { render partial: 'appended_review', locals: { review: @review }, formats: [:html] }
+      format.html { redirect_to restaurant_path(@restaurant, anchor: "review-#{@review.id}") }
+      format.text { render partial: 'appended_review', locals: { review: @review, restaurant: @restaurant }, formats: [:html] }
     end
   end
 
@@ -47,5 +48,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:rating, :title, :content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
