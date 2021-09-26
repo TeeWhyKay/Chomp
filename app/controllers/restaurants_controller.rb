f5a9d5f74a9d06fa34dad9fb94d0e68f6cd5d49e
jun wei <tjw1993@gmail.com>
2021-09-26 18:00:40 +0800

    implemented map function on restaurants page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,26 +6,6 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
-    
-    @geojson = {
-            type: 'Restaurant',
-            geometry: {
-              type: 'Point',
-              coordinates: [@restaurant.longitude, @restaurant.latitude]
-            },
-            properties: {
-              name: @restaurant.name,
-              address: @restaurant.address,
-              :'marker-color' => '#00607d',
-              :'marker-symbol' => 'circle',
-              :'marker-size' => 'medium'
-            }
-          }
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @geojson }  # respond with the created JSON object
-    end
   end
 
   def destroy
