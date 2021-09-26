d1eb666a93e6a1b287a62607d3b6bb8ee33bd675
Min Xuan <hi@ngminxuan.com>
2021-09-26 15:42:38 +0800

    email sends

--- app/mailers/restaurant_result_mailer.rb
+++ app/mailers/restaurant_result_mailer.rb
@@ -1,13 +1,20 @@
 class RestaurantResultMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.restaurant_result_mailer.result_release.subject
   #
   def result_release
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    restaurant_name = params[:restaurant].name
+    chomp_session = params[:chomp_session]
+    responses = Response.where(chomp_session: chomp_session.id)
+    responses.each do |response|
+      unless response.user.nil?
+        mail(
+          to: response.user.email,
+          subject: "#{chomp_session.name}'s decision is out! Enjoy your meetup at #{restaurant_name}."
+        )
+      end
+    end
   end
 end
