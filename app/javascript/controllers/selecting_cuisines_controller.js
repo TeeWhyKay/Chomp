42474a56c4f25711e570b2440789f75c21748f17
Min Xuan <hi@ngminxuan.com>
2021-09-26 09:41:49 +0800

    shows ticked after selecting

--- app/javascript/controllers/selecting_cuisines_controller.js
+++ app/javascript/controllers/selecting_cuisines_controller.js
@@ -1,12 +1,25 @@
+import { each } from "jquery";
 import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ['selection'];
 
-  select(event) {
-    const cuisineName = event.currentTarget.getAttribute("value").toLowerCase();
-    console.log(cuisineName);
+  connect() {
+    const initialCheckedStatus = document.querySelectorAll(".tag-selector[checked='checked']");
+    initialCheckedStatus.forEach(element => {
+      const cuisineLabel = element.nextSibling;
+      cuisineLabel.innerHTML = `<i class="fas fa-check-circle"></i>`;
+    });
+  }
 
-    // <label class="form-check-label collection_check_boxes" for="response_cuisine_japanese">okok</label>
+  select(event) {
+    const cuisineName = event.currentTarget.getAttribute("value");
+    const cuisineLabel = document.querySelector(`label[for="response_cuisine_${cuisineName.toLowerCase()}"]`);
+    if (cuisineName == cuisineLabel.innerHTML) {
+      cuisineLabel.innerHTML = `<i class="fas fa-check-circle"></i>`;
+    } else {
+      cuisineLabel.innerHTML = cuisineName;
+    }
+
   }
 }
