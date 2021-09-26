2ea9fac7e5bc943141ec55638eb77a3a0dfdd24b
Min Xuan <hi@ngminxuan.com>
2021-09-26 16:10:30 +0800

    control flow for no email

--- app/mailers/restaurant_result_mailer.rb
+++ app/mailers/restaurant_result_mailer.rb
@@ -5,14 +5,14 @@ class RestaurantResultMailer < ApplicationMailer
   #   en.restaurant_result_mailer.result_release.subject
   #
   def result_release
-    restaurant_name = params[:restaurant].name
-    chomp_session = params[:chomp_session]
-    responses = Response.where(chomp_session: chomp_session.id)
-    responses.each do |response|
+    @restaurant = params[:restaurant]
+    @chomp_session = params[:chomp_session]
+    @responses = Response.where(chomp_session: @chomp_session.id)
+    @responses.each do |response|
       unless response.user.nil?
         mail(
           to: response.user.email,
-          subject: "#{chomp_session.name}'s decision is out! Enjoy your meetup at #{restaurant_name}."
+          subject: "#{@chomp_session.name}'s decision is out! Enjoy your meetup at #{@restaurant.name}."
         )
       end
     end
