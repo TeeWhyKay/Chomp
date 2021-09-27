133ccc165d9e83b646208a439e28924f156553c2
Amelia Lim <amelialimyh@gmail.com>
2021-09-27 17:37:55 +0800

    removed index route and file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # test
-  resources :restaurants, only: %i[index show] do
+  resources :restaurants, only: :show do
     member do
       post 'toggle_favorite'
     end
