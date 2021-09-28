e8aa350d264a32b512717cc3964c7eea9d04252a
Min Xuan <hi@ngminxuan.com>
2021-09-28 12:51:04 +0800

    updated default chomp sess names

--- app/controllers/chomp_sessions_controller.rb
+++ app/controllers/chomp_sessions_controller.rb
@@ -16,7 +16,7 @@ class ChompSessionsController < ApplicationController
   def create
     @chomp_session = ChompSession.new(chomp_params)
     @chomp_session.user = current_user
-    @chomp_session.name = "Your Meeting Created on #{Time.now}" if @chomp_session.name == ""
+    @chomp_session.name = "I can't decide on a name on #{Time.now}" if @chomp_session.name == ""
     if @chomp_session.save
       job = GenerateRestaurantJob.set(wait_until: @chomp_session.session_expiry.hours.from_now).perform_later(@chomp_session.id)
       @chomp_session.sidekiq_jid = job.provider_job_id
