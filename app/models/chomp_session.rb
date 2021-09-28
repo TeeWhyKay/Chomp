ee7bbae0bf3506d2a34d0168dd4c5d7aada35b63
jun wei <tjw1993@gmail.com>
2021-09-28 11:46:20 +0800

    validation for past date/time implemented

--- app/models/chomp_session.rb
+++ app/models/chomp_session.rb
@@ -3,7 +3,7 @@ class ChompSession < ApplicationRecord
   generate_public_uid generator: PublicUid::Generators::NumberRandom.new(UID_RANGE)
 
   def self.find_puid(param)
-    find_by! public_uid: param.split('-').first
+    find_by public_uid: param.split('-').first
   end
 
   def to_param
@@ -14,6 +14,19 @@ class ChompSession < ApplicationRecord
   belongs_to :restaurant, optional: true
   has_many :responses
   validates :name, :date, :time, presence: true
+  validate :date_cannot_be_in_the_past, :time_cannot_be_in_the_past
   attribute :status, :string, default: "pending"
   attribute :session_expiry, :integer, default: 24
+
+  def date_cannot_be_in_the_past
+    if date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
+  def time_cannot_be_in_the_past
+    if date < Date.today && time.strftime("%H:%M").to_time < Time.now.strftime("%H:%M").to_time
+      errors.add(:time, "can't be in the past")
+    end
+  end
 end
