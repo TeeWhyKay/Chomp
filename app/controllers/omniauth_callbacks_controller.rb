bc5792b354d081b04f1cfc3c99e98d01d000bef8
Min Xuan <hi@ngminxuan.com>
2021-09-29 12:18:35 +0800

    add skip verification

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :google_oauth2
+
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
     # if @user.persisted?
