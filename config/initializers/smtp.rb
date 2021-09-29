5c3c97baef98660f3a038a3481552ff5cabab5eb
YongKeeTan <yongkee.t@gmail.com>
2021-09-29 23:02:38 +0800

    change smtp settings to mailgun

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.mailgun.org",
   port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  domain: 'letschomp.co',
+  user_name: ENV['MAILGUN_LOGIN'],
+  password: ENV['MAILGUN_PASSWORD'],
   authentication: :login,
   enable_starttls_auto: true
 }
