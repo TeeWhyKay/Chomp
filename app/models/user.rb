360eb6a3c67334b7582561d23b0704ae71b849f0
TeeWhyKay <72695565+TeeWhyKay@users.noreply.github.com>
2021-09-29 15:38:37 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,14 +31,4 @@ class User < ApplicationRecord
     end
     user
   end
-
-  # def self.from_omniauth(auth)
-  #   # Either create a User record or update it based on the provider (Google) and the UID
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.token = auth.credentials.token
-  #     user.expires = auth.credentials.expires
-  #     user.expires_at = auth.credentials.expires_at
-  #     user.refresh_token = auth.credentials.refresh_token
-  #   end
-  # end
 end
