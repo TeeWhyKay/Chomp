20f37a399b8deec8bbbd494532cbb139d60a9f87
Min Xuan <hi@ngminxuan.com>
2021-09-29 12:37:42 +0800

    rm extra sign in link & swap methods from tutorial

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,21 +3,21 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    # if @user.persisted?
-    #   flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-    #   sign_in_and_redirect @user, event: :authentication
-    # else
-    #   session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-    #   redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    # end
-
     if @user.persisted?
-      sign_in @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-    redirect_to '/'
+
+    # if @user.persisted?
+    #   sign_in @user, event: :authentication #this will throw if @user is not activated
+    #   set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    # else
+    #   session["devise.google_data"] = request.env["omniauth.auth"]
+    # end
+    # redirect_to '/'
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,28 +15,28 @@ class User < ApplicationRecord
     UserMailer.with(user: self).welcome.deliver_later
   end
 
-  # def self.from_omniauth(auth)
-  #   user = User.find_by(email: auth.info.email)
-  #   if user
-  #     user.provider = auth.provider
-  #     user.uid = auth.uid
-  #     user.save
-  #   else
-  #     user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #       user.email = auth.info.email
-  #       user.password = Devise.friendly_token[0,20]
-  #     end
-  #   end
-  #   user
-  # end
-
   def self.from_omniauth(auth)
-    # Either create a User record or update it based on the provider (Google) and the UID
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.token = auth.credentials.token
-      user.expires = auth.credentials.expires
-      user.expires_at = auth.credentials.expires_at
-      user.refresh_token = auth.credentials.refresh_token
+    user = User.find_by(email: auth.info.email)
+    if user
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    else
+      user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
     end
+    user
   end
+
+  # def self.from_omniauth(auth)
+  #   # Either create a User record or update it based on the provider (Google) and the UID
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.token = auth.credentials.token
+  #     user.expires = auth.credentials.expires
+  #     user.expires_at = auth.credentials.expires_at
+  #     user.refresh_token = auth.credentials.refresh_token
+  #   end
+  # end
 end
