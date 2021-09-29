321a845390e0e0ae70547f61e0e58426ad4e7b67
Min Xuan <hi@ngminxuan.com>
2021-09-29 14:29:43 +0800

    delete useless line

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,15 +10,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-
-    # if @user.persisted?
-    #   sign_in @user, event: :authentication #this will throw if @user is not activated
-    #   set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-    # else
-    #   session["devise.google_data"] = request.env["omniauth.auth"]
-    # end
-    # redirect_to '/'
-
   end
 
   def failure
