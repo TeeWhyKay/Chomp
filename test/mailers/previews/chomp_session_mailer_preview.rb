e21279df26e0b1837ada9bc8f2cda184ff384fea
Min Xuan <hi@ngminxuan.com>
2021-09-29 17:15:27 +0800

    deleted files responsible for email

--- test/mailers/previews/chomp_session_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/chomp_session_mailer
-class ChompSessionMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/chomp_session_mailer/create_chomp
-  def create_chomp
-    ChompSessionMailer.create_chomp
-  end
-
-end
