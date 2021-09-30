e38e065855347d974584702c3af92f4cff76f480
YongKeeTan <yongkee.t@gmail.com>
2021-09-30 11:29:04 +0800

    change auth to plain

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -4,6 +4,5 @@ ActionMailer::Base.smtp_settings = {
   domain: 'letschomp.co',
   user_name: ENV['MAILGUN_LOGIN'],
   password: ENV['MAILGUN_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
+  authentication: :plain
 }
