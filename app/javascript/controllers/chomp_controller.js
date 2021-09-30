e2101713926c95b6927fe00988d179c69be4bd74
Amelia Lim <amelialimyh@gmail.com>
2021-09-30 19:45:41 +0800

    resolved issue with person cap not being saved

--- app/javascript/controllers/chomp_controller.js
+++ app/javascript/controllers/chomp_controller.js
@@ -1,20 +1,49 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-static targets = ["alert", "deduct", "total", "add", "warning", "submit"]
+static targets = ["alert", "deduct", "total",
+                  "add", "warning", "submit", "inviteesinput"]
 
   connect() {
-    this.totalCount = 1;
+    console.log('connected!');
+    // if I am on new,
+    // url = chomp_sessions/new
+    if (window.location.pathname.includes('new')) {
+      this.totalCount = 1;
+    } else {
+      // url = chomp_sessions/4848393/edit
+      console.log('in the function');
+      let chompId = /\d+/.exec(window.location.pathname)[0]
+      fetch(`/chomp_sessions/${chompId}`, {
+            method: 'GET',
+            headers: { 'Accept': 'application/json' }
+        })
+        .then( res => res.json())
+        .then( data => this.updateInvitees(data.invitees))
+    }
+    // if I am on edit
+    // fetch the current number of people for chomp
+    // update totalCount
+    // update the innerText of totalTarget
+  }
+
+  updateInvitees(invitees) {
+    console.log(invitees);
+    this.totalCount = invitees;
+    this.inviteesinputTarget.setAttribute('value', invitees);
+    this.totalTarget.innerText = invitees;
   }
 
   increment() {
     this.totalCount += 1
+    this.inviteesinputTarget.setAttribute('value', this.totalCount);
     this.totalTarget.innerText = this.totalCount
     this.checkTotal()
   }
 
   decrement() {
     this.totalCount = this.totalCount - 1 == 0 ? 1 : this.totalCount - 1
+    this.inviteesinputTarget.setAttribute('value', this.totalCount);
     this.totalTarget.innerText = this.totalCount
     this.checkTotal()
   }
