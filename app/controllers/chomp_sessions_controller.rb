db16bf36b046d7578543f8327b515c825acf45df
jun wei <tjw1993@gmail.com>
2021-09-30 11:59:42 +0800

    timer implemented

--- app/controllers/chomp_sessions_controller.rb
+++ app/controllers/chomp_sessions_controller.rb
@@ -14,7 +14,7 @@ class ChompSessionsController < ApplicationController
   end
 
   def create
-    @chomp_session = ChompSession.new(chomp_params)
+  @chomp_session = ChompSession.new(chomp_params)
     @chomp_session.user = current_user
     @chomp_session.name = "I can't decide on a name on #{Time.now}" if @chomp_session.name == ""
     if @chomp_session.save
