099d718c67a200e90ef50e8d25ebac888d7e303c
YongKeeTan <yongkee.t@gmail.com>
2021-10-01 13:21:44 +0800

    force serve https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.force_ssl = true
 
 
   # Setting up mailer for delivery with gmail
