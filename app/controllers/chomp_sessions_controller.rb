fc90696d2ed0fa75841401b33ea9685f1604398a
YongKeeTan <yongkee.t@gmail.com>
2021-10-01 17:25:42 +0800

    hotfix redirect when results are out

--- app/controllers/chomp_sessions_controller.rb
+++ app/controllers/chomp_sessions_controller.rb
@@ -50,8 +50,11 @@ class ChompSessionsController < ApplicationController
     @search_exist = Response.where(user: current_user, chomp_session: @chomp_session)
     respond_to do |f|
       f.html do
-        redirect_to restaurant_path(@chomp_session.restaurant) if @chomp_session.status == "closed"
-        redirect_to chomp_session_response_url(@chomp_session,@search_exist.first) unless @search_exist == []
+        if @chomp_session.status == "closed"
+          redirect_to restaurant_path(@chomp_session.restaurant)
+        elsif @search_exist != []
+          redirect_to chomp_session_response_url(@chomp_session,@search_exist.first)
+        end
       end
       f.json { render json: @chomp_session }
     end
